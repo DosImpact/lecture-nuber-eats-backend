7ebb1f16b92a944f12530cfabf9241ea57d9f426
DosImpact <ypd03008@gmail.com>
2020-10-13 13:09:58 +0900

    init & first API

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+
+
+@Module({
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile: true,//join(process.cwd(), 'src/schema.gql'),
+  }), RestaurantsModule,],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
