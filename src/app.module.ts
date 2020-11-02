29b3d51792aa085b594643777e9d88c177fd68c0
DosImpact <ypd03008@gmail.com>
2020-11-02 11:49:49 +0900

    # app config server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 
 @Module({
-  imports: [GraphQLModule.forRoot({
-    autoSchemaFile: true,//join(process.cwd(), 'src/schema.gql'),
-  }), RestaurantsModule,],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test"
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,//join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      "type": "postgres",
+      "host": "localhost",
+      "port": 5433,
+      "username": "postgres",
+      "password": "dosimpact",
+      "database": "nuber-eats",
+      "synchronize": true, // db연결과 동시에 model migration 실행
+      "logging": true,
+    }),
+    RestaurantsModule,],
   controllers: [],
   providers: [],
 })
