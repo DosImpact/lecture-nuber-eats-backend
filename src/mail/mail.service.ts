454d71990b3731d2b55ef6209af6522e86795960
DosImpact <ypd03008@gmail.com>
2020-12-26 02:01:36 +0900

    mail function

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,11 +2,60 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { MailModuleOptions } from './mail.interface';
 
+import got from 'got';
+import * as FormData from 'form-data';
+
 @Injectable()
 export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {
     console.log('MailService options : ', options);
+    // this.sendEmail('ypd03008@naver.com', 'hello doyoung', '테스트 메일임');
+    this.sendEmail2('testing', 'test');
+  }
+  async sendEmail(to: string, subject: string, content: string) {
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('to', to);
+    form.append('subject', subject);
+    form.append('text', content);
+    try {
+      const response = await got(
+        `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+        {
+          headers: {
+            Authorization: `Basic ${Buffer.from(
+              `api:${this.options.apiKey}`,
+            ).toString('base64')}`,
+          },
+          body: form,
+          method: 'POST',
+        },
+      );
+      console.log(response.body);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  private async sendEmail2(subject: string, content: string) {
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('to', `ypd03008@naver.com`);
+    form.append('subject', subject);
+    form.append('text', content);
+    const response = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        body: form,
+      },
+    );
+    console.log(response.body);
   }
 }
