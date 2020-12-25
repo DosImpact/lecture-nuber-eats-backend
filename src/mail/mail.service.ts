50dcddbb4f9aa375b8e89878cd5dce916525e439
DosImpact <ypd03008@gmail.com>
2020-12-26 02:18:36 +0900

    mailgun template

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -12,38 +12,17 @@ export class MailService {
   ) {
     console.log('MailService options : ', options);
     // this.sendEmail('ypd03008@naver.com', 'hello doyoung', '테스트 메일임');
-    this.sendEmail2('testing', 'test');
+    this.sendEmail('testing', 'test');
   }
-  async sendEmail(to: string, subject: string, content: string) {
-    const form = new FormData();
-    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
-    form.append('to', to);
-    form.append('subject', subject);
-    form.append('text', content);
-    try {
-      const response = await got(
-        `https://api.mailgun.net/v3/${this.options.domain}/messages`,
-        {
-          headers: {
-            Authorization: `Basic ${Buffer.from(
-              `api:${this.options.apiKey}`,
-            ).toString('base64')}`,
-          },
-          body: form,
-          method: 'POST',
-        },
-      );
-      console.log(response.body);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  private async sendEmail2(subject: string, content: string) {
+
+  private async sendEmail(subject: string, content: string) {
     const form = new FormData();
     form.append('from', `Excited User <mailgun@${this.options.domain}>`);
     form.append('to', `ypd03008@naver.com`);
     form.append('subject', subject);
-    form.append('text', content);
+    form.append('template', 'nuber-eats');
+    form.append('v:username', 'nuber-eats username');
+    form.append('v:code', 'nuber-eats code');
     const response = await got(
       `https://api.mailgun.net/v3/${this.options.domain}/messages`,
       {
